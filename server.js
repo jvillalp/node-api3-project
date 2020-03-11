907e4da23446e2a4870b0dfecb941a08cfdca7f8
Juana Villalpando <juana.v14@gmail.com>
2020-03-11 14:59:53 -0700

    server is live

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 
+const PostRouter = require('./posts/postRouter');
+
 const server = express();
+
+server.use(express());
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
@@ -8,6 +12,8 @@ server.get('/', (req, res) => {
 
 //custom middleware
 
-function logger(req, res, next) {}
+// function logger(req, res, next) {}
+
+server.use('/api/posts', PostRouter)
 
 module.exports = server;
